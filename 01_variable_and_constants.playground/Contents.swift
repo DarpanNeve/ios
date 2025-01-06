9feb107caec1d448a2d06ec0b651276cf44f019c
darpanneve <darpanneve3@gmail.com>
2025-01-06 09:23:17 +0530

    minor updation

---
+++ 01_variable_and_constants.playground/Contents.swift
@@ -0,0 +1,12 @@
+// var is a keyword which can be changed
+// mutable
+var username="Darpan"
+
+// let is keyword using which a variable cannot be updated
+// immutable
+let accountId=123456789
+ print(accountId)
+var noOfBooks:Int=10
+ 
+var name,email:String
+
